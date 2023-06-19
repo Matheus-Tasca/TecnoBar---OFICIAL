faa096b266cbcbb0023cb0a401aca42c003dba7f
Matheus-Tasca <matheustascapecinatto@hotmail.com>
2023-06-19 00:25:19 -0300

    Todas as paginas conectadas e responsividade arrumada

--- inicio_dashboard_web/Contato - TCC/javasNavbarCorreto.js
+++
@@ -1,7 +0,0 @@
-const hamburguer = document.querySelector(".hamburguer");
-const Navmenu = document.querySelector(".nav-menu");
-
-hamburguer.addEventListener("click", ()  =>{
-    hamburguer.classList.toggle("active");
-    Navmenu.classList.toggle("active");
-})
