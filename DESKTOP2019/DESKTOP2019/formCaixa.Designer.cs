9b88badb271e7c69380df72aa536a63715d5c6e6
Matheus-Tasca <matheustascapecinatto@hotmail.com>
2023-10-08 19:37:37 -0300

    pdv cod search ready

--- DESKTOP2019/DESKTOP2019/formCaixa.Designer.cs
+++ DESKTOP2019/DESKTOP2019/formCaixa.Designer.cs
@@ -30,42 +30,42 @@ namespace DESKTOP2019
         private void InitializeComponent()
         {
             this.grpFiltrar = new System.Windows.Forms.GroupBox();
+            this.btnAdicionarProduto = new System.Windows.Forms.Button();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.lblValorTotal = new System.Windows.Forms.Label();
+            this.lblValorTotalTitulo = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblValorUnitario = new System.Windows.Forms.Label();
             this.lblValorUnitarioTitulo = new System.Windows.Forms.Label();
             this.txtNomeProduto = new System.Windows.Forms.TextBox();
             this.lblNomeProduto = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblCodigoBarras = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblCategoria = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.lblValorTotalTitulo = new System.Windows.Forms.Label();
-            this.lblValorTotal = new System.Windows.Forms.Label();
-            this.lblQuantidade = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.btnAdicionarProduto = new System.Windows.Forms.Button();
+            this.txtCodigoProduto = new System.Windows.Forms.TextBox();
+            this.lblCodigoProduto = new System.Windows.Forms.Label();
             this.btnCancelarVenda = new System.Windows.Forms.Button();
             this.btnFinalizarVenda = new System.Windows.Forms.Button();
             this.listBoxProdutos = new System.Windows.Forms.ListBox();
             this.lblSubTotalTitulo = new System.Windows.Forms.Label();
             this.lblSubTotal = new System.Windows.Forms.Label();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
             this.grpFiltrar.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpFiltrar
             // 
+            this.grpFiltrar.Controls.Add(this.txtCategoria);
             this.grpFiltrar.Controls.Add(this.btnAdicionarProduto);
-            this.grpFiltrar.Controls.Add(this.textBox2);
+            this.grpFiltrar.Controls.Add(this.txtQuantidade);
             this.grpFiltrar.Controls.Add(this.lblQuantidade);
             this.grpFiltrar.Controls.Add(this.lblValorTotal);
             this.grpFiltrar.Controls.Add(this.lblValorTotalTitulo);
-            this.grpFiltrar.Controls.Add(this.comboBox1);
             this.grpFiltrar.Controls.Add(this.lblCategoria);
-            this.grpFiltrar.Controls.Add(this.label1);
+            this.grpFiltrar.Controls.Add(this.lblValorUnitario);
             this.grpFiltrar.Controls.Add(this.lblValorUnitarioTitulo);
             this.grpFiltrar.Controls.Add(this.txtNomeProduto);
             this.grpFiltrar.Controls.Add(this.lblNomeProduto);
-            this.grpFiltrar.Controls.Add(this.textBox1);
-            this.grpFiltrar.Controls.Add(this.lblCodigoBarras);
+            this.grpFiltrar.Controls.Add(this.txtCodigoProduto);
+            this.grpFiltrar.Controls.Add(this.lblCodigoProduto);
             this.grpFiltrar.Location = new System.Drawing.Point(12, 23);
             this.grpFiltrar.Name = "grpFiltrar";
             this.grpFiltrar.Size = new System.Drawing.Size(402, 334);
@@ -73,6 +73,71 @@ namespace DESKTOP2019
             this.grpFiltrar.TabStop = false;
             this.grpFiltrar.Text = "Filtrar produtos";
             this.grpFiltrar.Enter += new System.EventHandler(this.grpFiltrar_Enter);
+            // 
+            // btnAdicionarProduto
+            // 
+            this.btnAdicionarProduto.Location = new System.Drawing.Point(25, 248);
+            this.btnAdicionarProduto.Name = "btnAdicionarProduto";
+            this.btnAdicionarProduto.Size = new System.Drawing.Size(316, 44);
+            this.btnAdicionarProduto.TabIndex = 12;
+            this.btnAdicionarProduto.Text = "Adicionar produto";
+            this.btnAdicionarProduto.UseVisualStyleBackColor = true;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(206, 135);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(77, 20);
+            this.txtQuantidade.TabIndex = 11;
+            this.txtQuantidade.KeyDown += new System.Windows.Forms.KeyEventHandler(this.enterQuantidadeItem);
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(203, 105);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(99, 13);
+            this.lblQuantidade.TabIndex = 10;
+            this.lblQuantidade.Text = "Quantidade do item";
+            // 
+            // lblValorTotal
+            // 
+            this.lblValorTotal.AutoSize = true;
+            this.lblValorTotal.Location = new System.Drawing.Point(283, 219);
+            this.lblValorTotal.Name = "lblValorTotal";
+            this.lblValorTotal.Size = new System.Drawing.Size(45, 13);
+            this.lblValorTotal.TabIndex = 9;
+            this.lblValorTotal.Text = "R$ 0,00";
+            this.lblValorTotal.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblValorTotalTitulo
+            // 
+            this.lblValorTotalTitulo.AutoSize = true;
+            this.lblValorTotalTitulo.Location = new System.Drawing.Point(283, 189);
+            this.lblValorTotalTitulo.Name = "lblValorTotalTitulo";
+            this.lblValorTotalTitulo.Size = new System.Drawing.Size(58, 13);
+            this.lblValorTotalTitulo.TabIndex = 8;
+            this.lblValorTotalTitulo.Text = "Valor Total";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(22, 108);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 6;
+            this.lblCategoria.Text = "Categoria";
+            this.lblCategoria.Click += new System.EventHandler(this.lblCategoria_Click);
+            // 
+            // lblValorUnitario
+            // 
+            this.lblValorUnitario.AutoSize = true;
+            this.lblValorUnitario.Location = new System.Drawing.Point(22, 219);
+            this.lblValorUnitario.Name = "lblValorUnitario";
+            this.lblValorUnitario.Size = new System.Drawing.Size(45, 13);
+            this.lblValorUnitario.TabIndex = 5;
+            this.lblValorUnitario.Text = "R$ 0,00";
+            this.lblValorUnitario.Click += new System.EventHandler(this.label1_Click);
             // 
             // lblValorUnitarioTitulo
             // 
@@ -100,94 +165,22 @@ namespace DESKTOP2019
             this.lblNomeProduto.TabIndex = 2;
             this.lblNomeProduto.Text = "Nome";
             // 
-            // textBox1
+            // txtCodigoProduto
             // 
-            this.textBox1.Location = new System.Drawing.Point(25, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(135, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtCodigoProduto.Location = new System.Drawing.Point(25, 66);
+            this.txtCodigoProduto.Name = "txtCodigoProduto";
+            this.txtCodigoProduto.Size = new System.Drawing.Size(135, 20);
+            this.txtCodigoProduto.TabIndex = 1;
+            this.txtCodigoProduto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pressionaEnter);
             // 
-            // lblCodigoBarras
+            // lblCodigoProduto
             // 
-            this.lblCodigoBarras.AutoSize = true;
-            this.lblCodigoBarras.Location = new System.Drawing.Point(22, 36);
-            this.lblCodigoBarras.Name = "lblCodigoBarras";
-            this.lblCodigoBarras.Size = new System.Drawing.Size(87, 13);
-            this.lblCodigoBarras.TabIndex = 0;
-            this.lblCodigoBarras.Text = "Codigo de barras";
-            this.lblCodigoBarras.Click += new System.EventHandler(this.lblCodigoBarras_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 219);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "R$ 0,00";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // lblCategoria
-            // 
-            this.lblCategoria.AutoSize = true;
-            this.lblCategoria.Location = new System.Drawing.Point(22, 108);
-            this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
-            this.lblCategoria.TabIndex = 6;
-            this.lblCategoria.Text = "Categoria";
-            this.lblCategoria.Click += new System.EventHandler(this.lblCategoria_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(25, 135);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 7;
-            // 
-            // lblValorTotalTitulo
-            // 
-            this.lblValorTotalTitulo.AutoSize = true;
-            this.lblValorTotalTitulo.Location = new System.Drawing.Point(283, 189);
-            this.lblValorTotalTitulo.Name = "lblValorTotalTitulo";
-            this.lblValorTotalTitulo.Size = new System.Drawing.Size(58, 13);
-            this.lblValorTotalTitulo.TabIndex = 8;
-            this.lblValorTotalTitulo.Text = "Valor Total";
-            // 
-            // lblValorTotal
-            // 
-            this.lblValorTotal.AutoSize = true;
-            this.lblValorTotal.Location = new System.Drawing.Point(283, 219);
-            this.lblValorTotal.Name = "lblValorTotal";
-            this.lblValorTotal.Size = new System.Drawing.Size(45, 13);
-            this.lblValorTotal.TabIndex = 9;
-            this.lblValorTotal.Text = "R$ 0,00";
-            this.lblValorTotal.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // lblQuantidade
-            // 
-            this.lblQuantidade.AutoSize = true;
-            this.lblQuantidade.Location = new System.Drawing.Point(203, 105);
-            this.lblQuantidade.Name = "lblQuantidade";
-            this.lblQuantidade.Size = new System.Drawing.Size(99, 13);
-            this.lblQuantidade.TabIndex = 10;
-            this.lblQuantidade.Text = "Quantidade do item";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(206, 135);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(77, 20);
-            this.textBox2.TabIndex = 11;
-            // 
-            // btnAdicionarProduto
-            // 
-            this.btnAdicionarProduto.Location = new System.Drawing.Point(25, 248);
-            this.btnAdicionarProduto.Name = "btnAdicionarProduto";
-            this.btnAdicionarProduto.Size = new System.Drawing.Size(316, 44);
-            this.btnAdicionarProduto.TabIndex = 12;
-            this.btnAdicionarProduto.Text = "Adicionar produto";
-            this.btnAdicionarProduto.UseVisualStyleBackColor = true;
+            this.lblCodigoProduto.AutoSize = true;
+            this.lblCodigoProduto.Location = new System.Drawing.Point(22, 36);
+            this.lblCodigoProduto.Name = "lblCodigoProduto";
+            this.lblCodigoProduto.Size = new System.Drawing.Size(94, 13);
+            this.lblCodigoProduto.TabIndex = 0;
+            this.lblCodigoProduto.Text = "Codigo do produto";
             // 
             // btnCancelarVenda
             // 
@@ -232,6 +225,13 @@ namespace DESKTOP2019
             this.lblSubTotal.Size = new System.Drawing.Size(0, 13);
             this.lblSubTotal.TabIndex = 17;
             // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(25, 135);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(135, 20);
+            this.txtCategoria.TabIndex = 13;
+            // 
             // formCaixa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -255,17 +255,16 @@ namespace DESKTOP2019
         #endregion
 
         private System.Windows.Forms.GroupBox grpFiltrar;
-        private System.Windows.Forms.Label lblCodigoBarras;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCodigoProduto;
+        private System.Windows.Forms.TextBox txtCodigoProduto;
         private System.Windows.Forms.Label lblValorUnitarioTitulo;
         private System.Windows.Forms.TextBox txtNomeProduto;
         private System.Windows.Forms.Label lblNomeProduto;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblValorUnitario;
         private System.Windows.Forms.Label lblCategoria;
         private System.Windows.Forms.Label lblValorTotalTitulo;
         private System.Windows.Forms.Button btnAdicionarProduto;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtQuantidade;
         private System.Windows.Forms.Label lblQuantidade;
         private System.Windows.Forms.Label lblValorTotal;
         private System.Windows.Forms.Button btnCancelarVenda;
@@ -273,5 +272,6 @@ namespace DESKTOP2019
         private System.Windows.Forms.ListBox listBoxProdutos;
         private System.Windows.Forms.Label lblSubTotalTitulo;
         private System.Windows.Forms.Label lblSubTotal;
+        private System.Windows.Forms.TextBox txtCategoria;
     }
 }
--- DESKTOP2019/DESKTOP2019/formCaixa.cs
+++ DESKTOP2019/DESKTOP2019/formCaixa.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,11 +21,6 @@ namespace DESKTOP2019
         }
 
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblCodigoBarras_Click(object sender, EventArgs e)
         {
 
         }
@@ -46,5 +44,71 @@ namespace DESKTOP2019
         {
 
         }
-    }
+
+        //Evento de enter ao digitar o codigo do produto
+        private void pressionaEnter(object sender, KeyEventArgs e)
+        {
+            //estabelecendo conexão com o banco de dados
+            String conString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            //query com o banco
+            String sqlSelect = "select nomeProd, codCategoria, valorVenda from produto where codProd = @codProd";
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    using (MySqlConnection con = new MySqlConnection(conString))
+                    {
+                        con.Open();
+                        MySqlCommand cmd = new MySqlCommand(sqlSelect, con);
+                        using (cmd)
+                        {
+                            cmd.Parameters.AddWithValue("@codProd", txtCodigoProduto.Text);
+
+                            using (MySqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    string nomeProduto = reader["nomeProd"].ToString();
+                                    string categoria = reader["codCategoria"].ToString();
+                                    if (categoria.Equals("1"))
+                                    {
+                                        categoria = "bebidas";
+                                    }
+                                    else{
+                                        categoria = "comidas";
+                                    }
+                                    string valorVenda = reader["valorVenda"].ToString();
+                                    txtNomeProduto.Text = nomeProduto;
+                                    txtCategoria.Text = categoria;
+                                    lblValorUnitario.Text = "R$ " + valorVenda;
+                                }
+                            }
+                        }
+                    }
+                } catch(SqlException ex)
+                {
+                    MessageBox.Show("ERRO NO SISTEMA :" + ex.Message,"ERRO");
+                }
+            e.SuppressKeyPress = true;
+            }
+
+        }
+
+        private void enterQuantidadeItem(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                int quantidade = int.Parse(txtQuantidade.Text);
+                double valorUnitario;
+                if(doubl.TryParse(lblValorUnitario.Text,out valorUnitario))
+                {
+                    Console.WriteLine(valorUnitario);
+                }
+
+                double total = quantidade * valorUnitario;
+                lblValorTotal.Text = "R$: " + total.ToString();
+            }
+        }
+    } 
 }
+
