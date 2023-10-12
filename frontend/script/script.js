a3951e78037c5977ecbaedc429d55f3ed4ee451e
Gustavo Vitali <gustavo.vitali17@gmail.com>
2023-10-12 00:31:47 -0300

    Visual Atualizado, ainda precisando ajustar o script usado na index

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,54 +1,78 @@
-const btnMostrarLogin = document.querySelector(".btnNavbar")
-const btnEscondeLogin = document.querySelector(".esconderLoginBtn")
-const btnLogin = document.querySelector(".btnLogin")
-const popLogin = document.querySelector("#popUp")
-const campoUsername = document.querySelector(".inputUsername")
-const campoPassword = document.querySelector(".inputSenha")
-const mensagemLogin = document.querySelector("#msg")
-const nomeInputContato = document.querySelector("#nomeInputContato")
-const emailInputContato = document.querySelector("#emailInputContato")
-const messageInputContato = document.querySelector("#messageInputContato")
-const btnSubmiteContato = document.querySelector(".btnSubmiteContato")
+// Novo Script
+document.addEventListener('DOMContentLoaded', function () {
+    const owlCarousel = document.querySelector('.owl-carousel');
 
-btnMostrarLogin.addEventListener('click',function(){
-    popLogin.showModal();
-})
-
-btnEscondeLogin.addEventListener('click', function (){
-    popLogin.close();
-})
-
-const login = () => {
-    let user = campoUsername.value
-    let pass = campoPassword.value
-    if ((user === "master") && (pass === "12345678")){
-        alert("LOGIN REALIZADO, BEM VINDO")
-        window.location.href = "InicioDashboard.html"
-     } else{
-         mensagemLogin.innerHTML = "ERRO - SENHA OU USUÁRIO INVÁLIDOS"
-         mensagemLogin.style.color = 'red';
-     }
-     console.log(campoUsername.value)
-     console.log(campoPassword.value)
-}
-
-btnLogin.addEventListener('click',(e)=>{
-    e.preventDefault()
-    login()
-})
-campoPassword.addEventListener('keyup',(e)=>{
-    if(e.code === 'Enter')
-        login()
-})
-
-btnSubmiteContato.addEventListener('click',()=>{
-    if(nomeInputContato.value == "" || emailInputContato.value == "" || messageInputContato.value == ""){
-        alert("Preencha todos os campos de contato!")
+    if (owlCarousel) { // Verifica si se encontró un elemento con la clase "owl-carousel"
+        // Configura el carrusel utilizando el método Owl Carousel
+        const owl = $(owlCarousel);
+        owl.owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                }
+            }
+        });
     }
-    else{
-        alert("Dados enviados com suceso !")
-        nomeInputContato.value = "";
-        emailInputContato.value = "";
-        messageInputContato.value = "";
-    }
-})
+    
+    
+});
+
+// Antigo script
+
+// const btnMostrarLogin = document.querySelector(".btnNavbar")
+// const btnEscondeLogin = document.querySelector(".esconderLoginBtn")
+// const btnLogin = document.querySelector(".btnLogin")
+// const popLogin = document.querySelector("#popUp")
+// const campoUsername = document.querySelector(".inputUsername")
+// const campoPassword = document.querySelector(".inputSenha")
+// const mensagemLogin = document.querySelector("#msg")
+// const nomeInputContato = document.querySelector("#nomeInputContato")
+// const emailInputContato = document.querySelector("#emailInputContato")
+// const messageInputContato = document.querySelector("#messageInputContato")
+// const btnSubmiteContato = document.querySelector(".btnSubmiteContato")
+
+// btnMostrarLogin.addEventListener('click',function(){
+//     popLogin.showModal();
+// })
+
+// btnEscondeLogin.addEventListener('click', function (){
+//     popLogin.close();
+// })
+
+// const login = () => {
+//     let user = campoUsername.value
+//     let pass = campoPassword.value
+//     if ((user === "master") && (pass === "12345678")){
+//         alert("LOGIN REALIZADO, BEM VINDO")
+//         window.location.href = "InicioDashboard.html"
+//      } else{
+//          mensagemLogin.innerHTML = "ERRO - SENHA OU USUÁRIO INVÁLIDOS"
+//          mensagemLogin.style.color = 'red';
+//      }
+//      console.log(campoUsername.value)
+//      console.log(campoPassword.value)
+// }
+
+// btnLogin.addEventListener('click',(e)=>{
+//     e.preventDefault()
+//     login()
+// })
+// campoPassword.addEventListener('keyup',(e)=>{
+//     if(e.code === 'Enter')
+//         login()
+// })
+
+// btnSubmiteContato.addEventListener('click',()=>{
+//     if(nomeInputContato.value == "" || emailInputContato.value == "" || messageInputContato.value == ""){
+//         alert("Preencha todos os campos de contato!")
+//     }
+//     else{
+//         alert("Dados enviados com suceso !")
+//         nomeInputContato.value = "";
+//         emailInputContato.value = "";
+//         messageInputContato.value = "";
+//     }
+// })
